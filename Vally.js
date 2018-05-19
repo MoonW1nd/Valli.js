34519a918335a6bc156e0c1fc2dd464abe4dcd4a
Alexander Ivankov <AlexanderIvankoff@gmail.com>
2018-05-19 10:47:46 +0300

    Add function validation type: String, Boolean, Undefined, NaN, Array'

--- Vally.js
+++ Vally.js
@@ -1,15 +1,41 @@
 (function( window ) {
   window.addEventListener('load', function () {
 
+    /*
+    * Check types functions
+    */
     var isNumber = function ( value ) {
       return toString.call( value ) === '[object Number]';
     }
 
+    var isString = function ( value ) {
+      return toString.call( value ) === '[object String]';
+    }
+
+    var isBoolean = function ( value ) {
+      return toString.call( value ) === '[object Boolean]';
+    }
+
+    var isUndefined = function ( value ) {
+      return toString.call( value ) === '[object Undefined]';
+    }
+
+    var isArray = function ( value ) {
+      return toString.call( value ) === '[object Array]';
+    }
+
+
+    /*
+    * Validate interface function
+    */
     var validateInterface = function( options, interfaceOptions ) {
       var interfaceIsValid = true;
 
       for ( let key in options ) {
-        if ( !interfaceOptions[ key ](options[ key ]) ) {
+        var interfaceValidationFunction = interfaceOptions[ key ];
+        var value = options[ key ]
+
+        if ( !interfaceValidationFunction( value ) ) {
           interfaceIsValid = false;
         }
       }
@@ -19,15 +45,20 @@
 
 
     var is = {
-      number: isNumber;
-      }
+      number: isNumber,
+      string: isString,
+      bool: isBoolean,
+      undefined: isUndefined,
+      NaN: isNaN,
+      array: isArray,
     }
 
+    // прокидываем переменные в глабальные данные
+    window.is = typeof window.is === 'undefined' ? is : console.warn('window.is reserved plese redefine Vally.is in new variable')
     window.Vally = {
       version: '0.0.1',
       is: is,
       validate: validateInterface;
     }
-
   })
 })( window )
